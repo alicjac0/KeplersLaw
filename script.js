f7fa44f66a096783a85b53c4d65855fa482ad7d6
alicjac0 <157830089+alicjac0@users.noreply.github.com>
2026-04-09 19:43:12 +0200

    controls update

--- script.js
+++ script.js
@@ -3,25 +3,27 @@ const ctx = canvas.getContext('2d')
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+let Pblur = 100
 
-var sun = {
+let sun = {
   x: canvas.width / 2,
   y: canvas.height / 2,
-  mass: 1000,
+  mass: 400,
 }
 
-var planet = {
-  x: sun.x + 500,
+let planet = {
+  x: sun.x + 200,
   y: sun.y,
   vx: 0,
-  vy: 1,
-  mass: 500,
+  vy: 1.2,
+  mass: 1,
 }
 
 function update() {
   let dx = sun.x - planet.x
   let dy = sun.y - planet.y
   let distance = Math.sqrt(dx * dx + dy * dy)
+  Pblur = 100 - distance / 1.25
   let force = sun.mass / (distance * distance)
 
   let ax = (force * dx) / distance
@@ -35,26 +37,61 @@ function update() {
 }
 
 function draw() {
-  ctx.fillStyle = '#07070733'
+  ctx.fillStyle = '#07070750'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
 
   ctx.beginPath()
-  ctx.arc(sun.x, sun.y, 20, 0, Math.PI * 2)
-  ctx.fillStyle = '#ffd000'
+  ctx.shadowBlur = 20
+  ctx.shadowColor = '#ffd000'
+  ctx.arc(sun.x, sun.y, 22, 0, Math.PI * 2)
+  ctx.fillStyle = '#ffdd47'
   ctx.fill()
   ctx.closePath()
+  ctx.shadowBlur = 0
 
   ctx.beginPath()
+  ctx.shadowBlur = Pblur
+  ctx.shadowColor = '#b83726'
   ctx.arc(planet.x, planet.y, 10, 0, Math.PI * 2)
-  ctx.fillStyle = '#da6767'
-  ctx.lineTo(planet.x, planet.y)
+  ctx.fillStyle = '#df3832'
   ctx.fill()
   ctx.closePath()
+  ctx.shadowBlur = 0
 }
+
+let animationId = null
 
 function loop() {
   update()
   draw()
-  requestAnimationFrame(loop)
+  animationId = requestAnimationFrame(loop)
 }
+
+function stopLoop() {
+  if (animationId !== null) {
+    cancelAnimationFrame(animationId)
+    animationId = null
+  }
+}
+
 loop()
+
+function reset() {
+  planet.vx = 0
+  planet.vy = 1
+  planet.x = sun.x + 200
+  planet.y = sun.y
+  sun.mass = 1000
+}
+
+function values() {
+  let sunMass = document.getElementById('sunMass').value
+  let pVelocity = document.getElementById('planetVelocity').value
+  let pX = document.getElementById('planetX').value
+  reset()
+  stopLoop()
+  sun.mass = Number(sunMass)
+  planet.vy = Number(pVelocity)
+  planet.x = sun.x + Number(pX)
+  loop()
+}
